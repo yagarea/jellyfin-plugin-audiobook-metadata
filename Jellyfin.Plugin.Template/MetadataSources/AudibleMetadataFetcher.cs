932a0d18aab4295fc6bf3617c0ffe6f3f1f7ebdf
Jan Černý <jc@ucw.cz>
2023-09-06 13:14:32 +0200

    remake tests

--- Jellyfin.Plugin.Template/MetadataSources/AudibleMetadataFetcher.cs
+++ Jellyfin.Plugin.Template/MetadataSources/AudibleMetadataFetcher.cs
@@ -2,6 +2,7 @@ using System;
 using System.Globalization;
 using System.Linq;
 using HtmlAgilityPack;
+using NUnit.Framework;
 
 namespace Jellyfin.Plugin.Template.MetadataSources;
 
@@ -12,6 +13,13 @@ public class AudibleMetadataFetcher
 {
     private const string BaseBookUrl = "https://www.audible.com/pd/";
     private const string SearchUrl = "https://www.audible.com/search?keywords=";
+
+
+    private const string TitleXPath = "/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[1]/h1";
+    private const string AuthorXPath = "/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[3]/a";
+    private const string PublisherXpath = "/html/body/div[1]/div[8]/div[10]/div/div/div[2]/span";
+    private const string NarratorsXPath = "/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[4]/a";
+    private const string overviewXPath = "/html/body/div[1]/div[8]/div[10]/div/div/div[1]/span";
 
     private static HtmlDocument GetAudiobookPageHtml(string audiobookId)
     {
@@ -31,7 +39,7 @@ public class AudibleMetadataFetcher
 
     private static string? GetTitle(HtmlDocument doc)
     {
-        return doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[1]/h1")[0].InnerText;
+        return doc.DocumentNode.SelectNodes(TitleXPath)[0].InnerText;
     }
 
     private static string? GetSortTitle(HtmlDocument doc)
@@ -41,19 +49,20 @@ public class AudibleMetadataFetcher
 
     private static string GetAuthor(HtmlDocument doc)
     {
-        HtmlNodeCollection authors = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li[3]/a");
+        HtmlNodeCollection authors = doc.DocumentNode.SelectNodes(AuthorXPath);
         string author = authors.Aggregate(string.Empty, (current, node) => current + (" " + node.InnerText));
         return author.Trim();
     }
 
     private static string? GetPublisher(HtmlDocument doc)
     {
-        return doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[30]/div/div/div[2]/span")[0].InnerText;
+        String publisher = doc.DocumentNode.SelectNodes(PublisherXpath)[0].InnerText;
+        return publisher.Trim();
     }
 
     private static string GetNarrator(HtmlDocument doc)
     {
-        HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[2]/div/div[3]/div/div/div/div[2]/span/ul/li");
+        HtmlNodeCollection linkNodes = doc.DocumentNode.SelectNodes(NarratorsXPath);
         string narrators = string.Empty;
         foreach (HtmlNode node in linkNodes) narrators += " " + node.InnerText;
         return narrators.Trim();
@@ -61,17 +70,19 @@ public class AudibleMetadataFetcher
 
     private static string GetOverview(HtmlDocument doc)
     {
-        HtmlNodeCollection summary = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[10]/div/div/div[1]/span");
+        HtmlNodeCollection summary = doc.DocumentNode.SelectNodes(overviewXPath);
         string overview = string.Empty;
         foreach (HtmlNode node in summary) overview += "\n\n" + node.InnerText;
         return overview.Trim();
     }
 
-    private static int? GetCommunityRating(HtmlDocument doc)
+    private static int GetCommunityRating(HtmlDocument doc)
     {
-        string rating = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[52]/div/div[2]/div[1]/span/ul/li[1]/span[3]")[0].InnerText;
+        string rating = doc.DocumentNode.SelectNodes("/html/body/div[1]/div[8]/div[52]/div/div[2]/div[1]/span/ul/li[1]/span[3]")[0].InnerText.Trim();
         rating = rating.Replace(" out of 5 stars", String.Empty);
-        return (int)(float.Parse(rating, CultureInfo.InvariantCulture.NumberFormat) * 2); // converting stars to x/10 score
+        string ratingValue = rating.Substring(0, 3);
+        TestContext.Out.WriteLine(ratingValue);
+        return (int) (double.Parse(ratingValue, CultureInfo.InvariantCulture) * 2);
     }
 
     private static string[] GetTags(HtmlDocument doc)
@@ -87,7 +98,7 @@ public class AudibleMetadataFetcher
     /// <summary>
     /// Fetch metadata for an audiobook from audible.
     /// </summary>
-    /// <param name="audiobookId">ID from url of book page.</param>
+    /// <param name="audiobookId">ID from url of book page.</paraB017V4IM1Gm>
     /// <returns>AudiobookMetadata object.</returns>
     public static AudioBookMetadata FetchMetadataById(string audiobookId)
     {
@@ -95,7 +106,7 @@ public class AudibleMetadataFetcher
         return new AudioBookMetadata(
             title: GetTitle(doc) ?? string.Empty,
             sortTitle: GetSortTitle(doc) ?? string.Empty,
-            communityRating: GetCommunityRating(doc).ToString() ?? string.Empty,
+            communityRating: Convert.ToString(GetCommunityRating(doc), "en-US") ?? string.Empty,
             overview: GetOverview(doc),
             narratedBy: GetNarrator(doc),
             publisher: GetPublisher(doc) ?? string.Empty,
@@ -129,5 +140,23 @@ public class AudibleMetadataFetcher
     public static AudioBookMetadata FetchMetadataByTitle(string title)
     {
         return FetchMetadataById(GetIdByTitle(title));
+    }
+
+
+    /// <summary>
+    /// Test that the title is fetched correctly.
+    /// </summary>
+    [Test]
+    public void TestHarryPotter()
+    {
+        HtmlDocument doc = GetAudiobookPageHtml("B017V4IM1G");
+        Assert.AreEqual("Harry Potter and the Sorcerer's Stone, Book 1", GetTitle(doc));
+        Assert.AreEqual("Harry Potter and the Sorcerer's Stone, Book 1", GetSortTitle(doc));
+        Assert.AreEqual("J.K. Rowling", GetAuthor(doc));
+        Assert.AreEqual("\u00a91997 J.K. Rowling (P)1999 Listening Library, an imprint of Penguin Random House Audio Publishing Group", GetPublisher(doc));
+        Assert.AreEqual("Jim Dale", GetNarrator(doc));
+        Assert.AreEqual("Harry Potter and the Sorcerer’s Stone will be streaming in Audible Plus through August 4th, 2023. Turning the envelope over, his hand trembling, Harry saw a purple wax seal bearing a coat of arms; a lion, an eagle, a badger and a snake surrounding a large letter 'H'. Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle. Then, on Harry's eleventh birthday, a great beetle-eyed giant of a man called Rubeus Hagrid bursts in with some astonishing news: Harry Potter is a wizard, and he has a place at Hogwarts School of Witchcraft and Wizardry. An incredible adventure is about to begin! Having become classics of our time, the Harry Potter stories never fail to bring comfort and escapism. With their message of hope, belonging and the enduring power of truth and love, the story of the Boy Who Lived continues to delight generations of new listeners.", GetOverview(doc));
+        Assert.AreEqual(8, GetCommunityRating(doc));
+        Assert.AreEqual(new string[] {"Science Fiction & Fantasy", "Fantasy", "Epic Fantasy"}, GetTags(doc));
     }
 }
